b5d23f67dc38542c9a32b0e6f630fca34ad42c70
Hugues Morisset <morisset.hugues@gmail.com>
2015-05-20 10:45:48 +0200

    Add ipv4 forwarding

--- iodined.sh
+++ iodined.sh
@@ -6,6 +6,7 @@ TUNNEL_IP=${IODINE_TUNNEL_IP:-"10.53.0.1/27"}
 mkdir -p /dev/net
 mknod /dev/net/tun c 10 200
 
+echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A  POSTROUTING -s $TUNNEL_IP -o eth0 -j MASQUERADE
 iptables -A FORWARD -i dns0 -j ACCEPT
 iptables -A FORWARD -i dns0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
