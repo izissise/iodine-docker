a9c900ce149d724f975f7a5913bc41da3adce52b
Hugues Morisset <morisset.hugues@gmail.com>
2015-05-26 00:04:38 +0200

    Comment useless lines

--- iodined.sh
+++ iodined.sh
@@ -4,15 +4,26 @@ TUNNEL_IP=${IODINE_TUNNEL_IP:-"10.53.0.1/27"}
 
 # Thanks to https://github.com/jpetazzo/dockvpn for the tun/tap fix
 mkdir -p /dev/net
-mknod /dev/net/tun c 10 200
+if [ ! -c /dev/net/tun ]; then
+    mknod /dev/net/tun c 10 200
+fi
 
-echo 1 > /proc/sys/net/ipv4/ip_forward
-iptables -t nat -A  POSTROUTING -s $TUNNEL_IP -o eth0 -j MASQUERADE
-iptables -A FORWARD -i dns0 -j ACCEPT
-iptables -A FORWARD -i dns0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i eth0 -o dns0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -t nat -A POSTROUTING -s $TUNNEL_IP -o eth0 -j MASQUERADE
+# iptables -t nat -C POSTROUTING -s $TUNNEL_IP -o eth0 -j MASQUERADE || {
+#   
+#   }
 
-
+# iptables -C FORWARD -i dns0 -j ACCEPT || { 
+#   iptables -A FORWARD -i dns0 -j ACCEPT
+#   }
+#   
+# iptables -C FORWARD -i dns0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT || {
+#   iptables -A FORWARD -i dns0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+#   }
+#  
+# iptables -C FORWARD -i eth0 -o dns0 -m state --state RELATED,ESTABLISHED -j ACCEPT || {
+#   iptables -A FORWARD -i eth0 -o dns0 -m state --state RELATED,ESTABLISHED -j ACCEPT  
+#   }
 
 while [ 1 ]
 do
